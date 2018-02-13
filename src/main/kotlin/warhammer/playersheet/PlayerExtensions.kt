5b719a0ec562e720cc8e27a1be9197db29f192a0
Clement Nicolas <clement.nicolas-ext@amundi.com>
2018-02-13 16:54:20 +0100

    start test of player manipulations

--- src/main/kotlin/warhammer/playersheet/PlayerExtensions.kt
+++ src/main/kotlin/warhammer/playersheet/PlayerExtensions.kt
@@ -5,3 +5,12 @@ import warhammer.database.entities.player.Player
 import warhammer.database.entities.player.characteristics.Characteristic
 
 fun Player.createHand(characteristic: Characteristic, name: String = "Hand"): Hand = characteristics[characteristic].getHand(name)
+
+fun Player.earnExperiencePoints(experiencePoints: Int): Player =
+        this.copy(
+                state = state.copy(
+                        career = state.career.copy(
+                                totalExperience = state.career.totalExperience!! + experiencePoints,
+                                availableExperience = state.career.totalExperience!! + experiencePoints)
+                )
+        )
---
+++ src/test/kotlin/warhammer/playersheet/player/PlayerManipulationTest.kt
@@ -0,0 +1,21 @@
+package warhammer.playersheet.player
+
+import org.assertj.core.api.Assertions.assertThat
+import org.testng.annotations.Test
+import warhammer.database.entities.player.Player
+import warhammer.playersheet.earnExperiencePoints
+
+class PlayerManipulationTest {
+    @Test
+    fun should_earn_an_experience_point() {
+        var player = Player("PlayerName")
+
+        assertThat(player.totalExperience).isEqualTo(0)
+        assertThat(player.availableExperience).isEqualTo(0)
+
+        player = player.earnExperiencePoints(1)
+
+        assertThat(player.totalExperience).isEqualTo(1)
+        assertThat(player.availableExperience).isEqualTo(1)
+    }
+}
