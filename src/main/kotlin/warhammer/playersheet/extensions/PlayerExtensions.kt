22b755e55022d2cba69901ffe4cf968fd787bf8f
Clement Nicolas <clement.nicolas-ext@amundi.com>
2018-02-28 17:15:04 +0100

    upgrade database, new extension to create a hand from a skill

--- src/main/kotlin/warhammer/playersheet/extensions/PlayerExtensions.kt
+++ src/main/kotlin/warhammer/playersheet/extensions/PlayerExtensions.kt
@@ -1,10 +1,24 @@
 package warhammer.playersheet.extensions
 
+import warhammer.database.entities.hand.DifficultyLevel
 import warhammer.database.entities.hand.Hand
 import warhammer.database.entities.player.Player
 import warhammer.database.entities.player.enums.Characteristic
+import warhammer.database.entities.player.playerLinked.skill.Skill
 
-fun Player.createHand(characteristic: Characteristic, name: String = "Hand"): Hand = this[characteristic].getHand(name)
+fun Player.createHand(characteristic: Characteristic,
+                      name: String = "Hand",
+                      difficultyLevel: DifficultyLevel = DifficultyLevel.NONE): Hand =
+        this[characteristic].getHand(name, difficultyLevel)
+
+fun Player.createHand(skill: Skill,
+                      name: String = "Hand",
+                      difficultyLevel: DifficultyLevel = DifficultyLevel.NONE): Hand {
+    val hand = this.createHand(skill.characteristic, name, difficultyLevel)
+    hand.expertiseDicesCount = skill.level
+
+    return hand
+}
 
 fun Player.earnExperiencePoints(experiencePoints: Int): Player {
     availableExperience += experiencePoints
--- src/test/kotlin/warhammer/playersheet/player/PlayerHandTest.kt
+++ src/test/kotlin/warhammer/playersheet/player/PlayerHandTest.kt
@@ -2,9 +2,13 @@ package warhammer.playersheet.player
 
 import org.assertj.core.api.Assertions.assertThat
 import org.testng.annotations.Test
+import warhammer.database.PlayerFacade
+import warhammer.database.entities.hand.DifficultyLevel.HARD
 import warhammer.database.entities.player.CharacteristicValue
 import warhammer.database.entities.player.Player
 import warhammer.database.entities.player.enums.Characteristic
+import warhammer.database.entities.player.extensions.getSkillByName
+import warhammer.playersheet.PlayerSheetContext
 import warhammer.playersheet.extensions.createHand
 
 class PlayerHandTest {
@@ -13,9 +17,23 @@ class PlayerHandTest {
         val player = Player(name = "PlayerName", strength = CharacteristicValue(3, 1))
 
         val hand = player.createHand(Characteristic.STRENGTH, "Hand")
-        assertThat(hand).isNotNull()
         assertThat(hand.name).isEqualTo("Hand")
         assertThat(hand.characteristicDicesCount).isEqualTo(3)
         assertThat(hand.fortuneDicesCount).isEqualTo(1)
+    }
+
+    @Test
+    fun should_create_hand_from_skill() {
+        val facade = PlayerFacade(PlayerSheetContext.DATABASE_URL, PlayerSheetContext.DRIVER)
+        val player = facade.save(Player("John"))
+        player.strength = CharacteristicValue(4,2)
+        player.getSkillByName("capacité de combat")!!.level = 2
+        val hand = player.createHand(player.getSkillByName("capacité de combat")!!, "HandName", HARD)
+
+        assertThat(hand.name).isEqualTo("HandName")
+        assertThat(hand.characteristicDicesCount).isEqualTo(4)
+        assertThat(hand.fortuneDicesCount).isEqualTo(2)
+        assertThat(hand.expertiseDicesCount).isEqualTo(2)
+        assertThat(hand.challengeDicesCount).isEqualTo(HARD.ordinal)
     }
 }
